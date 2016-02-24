7c2f754d6d88d3eb08027c4d9cc46093559b6907
Gastón Ambrogi <gaston.ambrogi@gmail.com>
2016-02-24 19:02:16 -0300

    Implemented Inflect task for service generation.

---
+++ lib/generators/service_generator.rb
@@ -0,0 +1,21 @@
+require "thor"
+require 'pry'
+
+class ServiceGenerator < Thor::Group
+	include Thor::Actions
+
+	def self.source_root
+		File.join(File.dirname(__FILE__), 'templates')
+	end
+
+	argument :name
+	def generate
+		template('service.tt', "lib/inflect/services/#{name}_service.rb")
+	end
+end
+
+class CLI < Thor
+	register ServiceGenerator, "generate:service", "generate:service <service_name>", "Generates a service skeleton."
+end
+
+CLI.start(ARGV)
