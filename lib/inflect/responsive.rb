e8ed2732548e5de43e5d4161892c26d35e4df57c
Gastón Ambrogi <gaston.ambrogi@gmail.com>
2016-03-11 14:48:21 -0300

    Changed Responsive#respond! visibility from public to protected.

--- lib/inflect/responsive.rb
+++ lib/inflect/responsive.rb
@@ -12,14 +12,6 @@ module Inflect
       validate_response(Inflect::Response.new(content, opts))
     end
 
-    # Method that creates Response instance, skipping response validation.
-    # @param content [String, Hash] the response of the service.
-    # @return [Inflect::Response]
-    def respond!(content, options = {})
-      opts = merge_options(options)
-      Inflect::Response.new(content, opts)
-    end
-
     # Method that ensures that the service calls respond method.
     # @param words [Array<String>]
     # @return [Inflect::Response, nil] Returns nil if response is not valid.
@@ -27,6 +19,17 @@ module Inflect
       service_response = handle(words)
       raise "Invalid Response Type. Expected nil or Inflect::Response and returned #{ service_response.class }" if !((service_response.is_a? Inflect::Response) || service_response.nil?)
       service_response
+    end
+
+    protected
+
+    # Method that creates Response instance, skipping response validation.
+    # Only for debugging.
+    # @param content [String, Hash] the response of the service.
+    # @return [Inflect::Response]
+    def respond!(content, options = {})
+      opts = merge_options(options)
+      Inflect::Response.new(content, opts)
     end
 
     private
