fa3676cb067321813bfcd84c4d0b58aca5ee38d2
Santiago Figueiras <santi.figueiras95@gmail.com>
2016-03-24 15:48:57 -0300

    Added capability for generating more than one word service, now name has to be supplied in camelcase. Closes #4.

--- lib/generators/service_generator.rb
+++ lib/generators/service_generator.rb
@@ -1,19 +1,28 @@
-require "thor"
+require 'thor'
+require 'pry'
+
+require 'inflect/inflector'
 
 # Service Generator Class.
 class ServiceGenerator < Thor::Group
 	include Thor::Actions
 
+	# Include Inflector for string management utils such
+	# as camelize.
+	using Inflect::Inflector
+	
 	# Defines the templates path.
 	def self.source_root
 		File.join(File.dirname(__FILE__), 'templates')
 	end
 
+	# The parameter name in CamelCase.
 	argument :name
+
 	# Responsible to generate the named service inside lib/services path
 	# of working directory.
 	def generate
-		template('service.tt', "lib/services/#{name}_service.rb")
+		template('service.tt', "lib/services/#{name.underscore}_service.rb")
 	end
 end
 
